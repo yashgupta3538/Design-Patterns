ca429f83c83b10e575fabe16c70943b014093ff7
Yashasvi Gupta <gyashasvi85@gmail.com>
2026-08-01 19:39:13 +0530

    chore: shalloww and deep copy illustration

--- Prototype/Main.java
+++ Prototype/Main.java
@@ -3,15 +3,28 @@ public class Main {
         NetworkConnection networkConnection = new NetworkConnection();
 
         networkConnection.setIp("192.168.0.1");
-        networkConnection.setData("Data which takes time to load!!");
+        networkConnection.setData("Data which takes time to load");
+        networkConnection.loadDomains();
 
         System.out.println(networkConnection);
 
         try {
             NetworkConnection networkConnection2 = (NetworkConnection) networkConnection.clone();
+            networkConnection.getDomains().remove(0);
+            System.out.println(networkConnection);
             System.out.println(networkConnection2);
         } catch (CloneNotSupportedException e) {
             System.out.print(e.getMessage());
         }
     }
 }
+
+// O/P with shallow copy
+// 192.168.0.1 Data which takes time to load:[www.yahoo.com, www.google.com, www.stackoverflow.com]
+// 192.168.0.1 Data which takes time to load:[www.google.com, www.stackoverflow.com]
+// 192.168.0.1 Data which takes time to load:[www.google.com, www.stackoverflow.com]
+
+// O/P with deep copy
+// 192.168.0.1 Data which takes time to load:[www.yahoo.com, www.google.com, www.stackoverflow.com]
+// 192.168.0.1 Data which takes time to load:[www.google.com, www.stackoverflow.com]
+// 192.168.0.1 Data which takes time to load:[www.yahoo.com, www.google.com, www.stackoverflow.com]
--- Prototype/NetworkConnection.java
+++ Prototype/NetworkConnection.java
@@ -1,9 +1,17 @@
-public class NetworkConnection implements Cloneable{
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkConnection implements Cloneable {
     private String ip;
     private String data;
+    private List<String> domains = new ArrayList<>();
 
     public NetworkConnection() {
+        
+    }
 
+    public List<String> getDomains() {
+        return this.domains;
     }
 
     public String getIp() {
@@ -12,6 +20,10 @@ public class NetworkConnection implements Cloneable{
 
     public String getData() {
         return this.data;
+    }
+
+    public void setDomains(List<String> domains) {
+        this.domains = domains;
     }
 
     public void setIp(String ip) {
@@ -23,10 +35,27 @@ public class NetworkConnection implements Cloneable{
     }
 
     public String toString() {
-        return this.ip + " " + this.data;
+        return this.ip + " " + this.data + ":" + this.domains;
     }
 
     public Object clone() throws CloneNotSupportedException {
-        return this;
+
+        // custom logic to implement deep copy
+        NetworkConnection connection = new NetworkConnection();
+        connection.setIp(this.getIp());
+        connection.setData(this.getData());
+        for (String str : this.getDomains()) {
+            connection.getDomains().add(str);
+        }
+        return connection;
+
+        // By default Shallow copy will be done
+        // return this;
+    }
+
+    public void loadDomains() {
+        domains.add("www.yahoo.com");
+        domains.add("www.google.com");
+        domains.add("www.stackoverflow.com");
     }
 }
